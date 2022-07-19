4c209934f5949eba8a64e5d064ac9e85853f1cf9
papasmf1 <papasmf1@gmail.com>
2022-07-19 13:51:38 +0900

    이름 충돌 버그 수정

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,11 +1,15 @@
+#전역변수
 str = "Not Class Member"
+
+#클래스 정의:파이썬은 모호한 것보다는 명확한 것이 좋다! 
 class GString:
     def __init__(self):
+        #인스턴스 멤버 변수
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
+        print(self.str)
 
 g = GString()
 g.set("First Message")
