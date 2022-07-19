a63d468b3fae7a83311e0a47998ac3294637912f
papasmf1 <papasmf1@gmail.com>
2022-07-19 15:37:01 +0900

    클래스 연습 완료

--- 상속01.py
+++ 상속01.py
@@ -1,22 +1,28 @@
+#부모 클래스 정의
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
+#자식 클래스 정의
 class Student(Person):
+    #초기화 메서드 재정의
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #부모의 생성자 호출
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
+    #상속받아서 메서드 재정의(override)
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(self.subject, self.studentID))
 
-
+#인스턴스 생성
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+s = Student("이순신", "010-111-1234", "빅데이터", "191122")
+p.printInfo()
+s.printInfo() 
 
 
