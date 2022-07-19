5293cdaa8e46ca7937f3323bb556a3089d85eb73
papasmf1 <papasmf1@gmail.com>
2022-07-19 13:40:10 +0900

    클래스 연습2

--- class1.py
+++ class1.py
@@ -9,7 +9,16 @@ class Person:
 
 #2)인스턴스 
 p1 = Person()
+p2 = Person() 
+p1.name = "전우치"
 #3)인스턴스 메서드 호출
 p1.print() 
+p2.print() 
+#동적으로 변수 추가
+Person.title = "new title"
+print(p1.title)
+print(p2.title)
+print(Person.title)
+
 
 
