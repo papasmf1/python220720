1e070d37b8b0e0330cc27619734f50841ca238ba
papasmf1 <papasmf1@gmail.com>
2022-07-19 14:53:06 +0900

    클래스 연습 7

--- BankAccount.py
+++ BankAccount.py
@@ -3,18 +3,26 @@
 #은행의 계정을 표현한 클래스 
 class BankAccount:
     def __init__(self, id, name, balance):
-        self.id = id
-        self.name = name 
-        self.balance = balance 
+        #클래스 내부에 숨김(이름 규칙)
+        self.__id = id
+        self.__name = name 
+        self.__balance = balance 
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
     def __str__(self):
-        return "{0} , {1} , {2}".format(self.id, \
-            self.name, self.balance)
+        return "{0} , {1} , {2}".format(self.__id, \
+            self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
+account1.deposit(5000)
 account1.withdraw(3000)
 print(account1)
+#print(account1.__balance)
+#외부에서는 이름이 변경
+#외부에서 접근하는 백도어(테스트 용도)
+print(account1._BankAccount__balance)
+
+
