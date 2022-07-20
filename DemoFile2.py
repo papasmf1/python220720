7fbe7eefe949955c9c04d512c7c4bc910d68f96b
papasmf1 <papasmf1@gmail.com>
2022-07-20 11:51:16 +0900

    파일 연습 3

---
+++ DemoFile2.py
@@ -0,0 +1,33 @@
+# DemoFile2.py 
+
+#파일을 생성 
+f = open("demo.txt", "wt")
+f.write("첫번째\n두번째\n세번째\n")
+f.close()
+
+#파일을 읽어서 처리
+f = open("demo.txt", "rt")
+result = f.read()
+print(result)
+print(f.tell())
+f.seek(0)
+lst = f.readlines()
+print(lst)
+for item in lst:
+    print(item, end="")
+
+#처음으로 리셋
+f.seek(0)
+print(f.readline(), end="")
+print(f.readline(), end="")
+f.close() 
+
+#with구문을 같이 사용하는 경우
+with open("demo.txt", "rt") as f:
+    for item in f.readlines():
+        print(item, end="")
+
+
+
+
+
