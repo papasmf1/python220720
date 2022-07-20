40845f43149ab3043810d44305ab3911a933f4a9
papasmf1 <papasmf1@gmail.com>
2022-07-20 11:24:31 +0900

    파일 연습 2

--- DemoFile.py
+++ DemoFile.py
@@ -3,7 +3,7 @@ print("---정렬방식---")
 for x in range(1,6):
     print(x,"*",x,"=",x*x)
 
-print("---왼쪽정렬---")
+print("---오른쪽정렬---")
 for x in range(1,6):
     print(x,"*",x,"=", str(x*x).rjust(3))
 
@@ -12,4 +12,17 @@ for x in range(1,6):
     print(x,"*",x,"=",str(x*x).zfill(3))
 
 
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+print("{0:,}".format(15000000))
+print("{0:e}".format(4/3))
+print("{0:f}".format(4/3))
+print("{0:.2f}".format(4/3))
 
+url = "http://www.credu.com/?page=" + str(1) 
+print(url)
+
+#파일을 생성 
+f = open("demo.txt", "wt")
+f.write("첫번째\n두번째\n세번째\n")
+f.close()
