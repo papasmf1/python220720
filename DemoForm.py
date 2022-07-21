b94dc180678d6882ff51f32bc445ebcbd587163f
papasmf1 <papasmf1@gmail.com>
2022-07-21 14:38:25 +0900

    첫번째 GUI화면

---
+++ DemoForm.py
@@ -0,0 +1,22 @@
+# DemoForm.py 
+import sys 
+from PyQt5.QtWidgets import * 
+from PyQt5 import uic 
+
+#폼디자인을 로딩
+form_class = uic.loadUiType("DemoForm.ui")[0]
+
+#클래스 정의
+class DemoForm(QDialog, form_class):
+    #초기화 메서드
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.label.setText("첫번째 Qt화면")
+
+#진입점을 체크
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demoWindow = DemoForm()
+    demoWindow.show() 
+    app.exec_() 
